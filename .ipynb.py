3cdbafa7b652fe06291ba3060f01e8d56f515292
sriram1498 <68559781+sriram1498@users.noreply.github.com>
2020-07-21 22:11:09 -0700

    Add files via upload

---
+++ .ipynb.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#helloworld
+"""hello sir"""
+
+
+# In[9]:
+
+
+print("hello      world")
+
+
+# In[10]:
+
+
+print("python's world")
+
+
+# # spaces
+
+# In[11]:
+
+
+print("python\tworld")
+
+
+# # new line
+
+# In[20]:
+
+
+print("Hello\nworld")
+
+
+# # formated output
+
+# In[13]:
+
+
+name="sriram"
+age=22
+marks=85.66
+print(f"The name of person is {name} age is {age} the marks is {marks}")
+
+
+# In[19]:
+
+
+print("The name of person is %10s marks is %10.2f age is %10d"%(name,marks,age))
+
+
+# In[17]:
+
+
+5**5
+
+
+# In[18]:
+
+
+10+20
+
+
+# In[21]:
+
+
+10-20
+
+
+# In[23]:
+
+
+10/2
+
+
+# In[24]:
+
+
+10/3
+
+
+# In[25]:
+
+
+10%3
+
+
+# In[26]:
+
+
+10//3
+
+
+# In[27]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[28]:
+
+
+c=10
+a==c
+
+
+# In[29]:
+
+
+c<=a
+
+
+# In[30]:
+
+
+a>=b
+
+
+# In[31]:
+
+
+a!=b
+
+
+# In[32]:
+
+
+str1="pythonprogramming"
+"x" in str1
+
+
+# In[33]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[34]:
+
+
+a is b
+
+
+# In[35]:
+
+
+id(a)
+
+
+# In[36]:
+
+
+id(b)
+
+
+# In[37]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[38]:
+
+
+x is y
+
+
+# In[39]:
+
+
+id(x)
+
+
+# In[40]:
+
+
+id(y)
+
+
+# In[41]:
+
+
+a=257
+b=257
+a==b
+
+
+# In[42]:
+
+
+a is b
+
+
+# In[43]:
+
+
+10>8
+
+
+# In[44]:
+
+
+8>9
+
+
+# In[ ]:
+
+
+
+
